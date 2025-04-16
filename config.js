69bec7e2eb813db8c97413fa840ff92b9e6a803b
theschoolshopowner <blake@dekelco.com>
2025-04-15 21:43:26 -0700

    Added improved shop

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+// These values come from your project files
+const API_KEY = 'AIzaSyDqd1fC7NRMLVMDHMvhNtZC5O8rJqjNNeE';
+const SHEET_ID = '13DiBvVe-jNM_o2CjdZXm34jLS0konxycmssb4BKpx8k';
+const SHEET_NAME = 'Sheet1';
