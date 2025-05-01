761e299993f9a8fffdb19eec27a863dcd07dfa86
theschoolshopowner <blake@dekelco.com>
2025-04-30 22:30:11 -0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-// ——————————————
-// Google Sheets configuration
-const API_KEY = 'AIzaSyDqd1fC7NRMLVMDHMvhNtZC5O8rJqjNNeE';
-const SHEET_ID = '13DiBvVe-jNM_o2CjdZXm34jLS0konxycmssb4BKpx8k';
-const SHEET_NAME = 'Sheet1';
